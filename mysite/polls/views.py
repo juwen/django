3af87747411de279bfc524eee97f405ecb032357
juwen <juwen.zh@gmail.com>
2017-05-14 17:09:37 +0800

    Improving our view --bug

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
-from django.urls import timezone, reverse
+from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
